4799d85422c3d1f98cd14a667b66fae3acd0c209
Riyathakur235 <riyathakur8052@gmail.com>
2026-05-12 21:04:24 +0530

    sort

---
+++ Binary Search/Split_largest.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+# THIS PROBLEM IS SAME AS BOOK ALLOCATION PROBLEM!
+
+
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        def isValid(limit: int) -> bool:
+            curr, count = 0, 1
+            for item in nums:
+                if item > limit:
+                    return False
+                if curr + item <= limit:
+                    curr += item
+                else:
+                    count += 1
+                    curr = item
+            return count <= k
+
+        low, high = min(nums), sum(nums)
+        ans = -1
+        while low <= high:
+            mid = (low + high) // 2
+            if isValid(mid):
+                ans = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+        return ans
+
+nums = [7, 2, 5, 10, 8]
+k = 2
+
+sol = Solution()
+print(sol.splitArray(nums, k))    
