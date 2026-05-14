53c0f2792708bf8f0cfd33ef9e2401d6a9d1bd9e
Riyathakur235 <riyathakur8052@gmail.com>
2026-05-14 22:40:15 +0530

    skip

--- Binary Search/Ship_Package.py
+++ Binary Search/Ship_Package.py
@@ -26,7 +26,13 @@ class Solution:
                 low = mid + 1
         return ans
     
-    
+weights = [1,2,3,4,5,6,7,8,9,10]
+days = 5
+
+obj = Solution()
+result = obj.shipWithinDays(weights, days)
+
+print(result)   
     
     
 '''
