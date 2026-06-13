18989b4d5357b28e0c249a81f9139e8c2eb18cf2
Riyathakur235 <riyathakur8052@gmail.com>
2026-06-13 06:20:26 +0530

    max

--- Binary Search/Max_1st_Row.py
+++ Binary Search/Max_1st_Row.py
@@ -10,3 +10,22 @@ class Solution:
             if count > maxi:
                 maxi, index = count, i
         return index
+    
+# Input matrix
+arr = [
+    [0, 1, 1, 1],
+    [0, 0, 1, 1],
+    [1, 1, 1, 1],
+    [0, 0, 0, 0]
+]
+
+n = len(arr)
+m = len(arr[0])
+
+# Create object
+obj = Solution()
+
+# Call function
+result = obj.rowWithMax1s(arr, n, m)
+
+print("Row with maximum 1s:", result)    
