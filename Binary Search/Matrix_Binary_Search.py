54fee6789a99c5ed5db0252c1e535107df3c093b
Riyathakur235 <riyathakur8052@gmail.com>
2026-06-14 21:34:16 +0530

    matrix

--- Binary Search/Matrix_Binary_Search.py
+++ Binary Search/Matrix_Binary_Search.py
@@ -19,3 +19,15 @@ class Solution:
             else:
                 low = mid + 1
         return False
+    
+# Input
+matrix = [
+    [1, 3, 5, 7],
+    [10, 11, 16, 20],
+    [23, 30, 34, 60]
+]
+
+target = 3
+obj = Solution()
+result = obj.searchMatrix(matrix, target)
+print(result)    
