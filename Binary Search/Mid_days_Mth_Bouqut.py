e1332bad85f0ee3991b764673de446aa6d89831b
Riyathakur235 <riyathakur8052@gmail.com>
2026-06-20 21:35:01 +0530

    mid

---
+++ Binary Search/Mid_days_Mth_Bouqut.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+class Solution:
+    def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
+        def isValid(mid):
+            count = temp = 0
+            for item in bloomDay:
+                if item <= mid:
+                    temp += 1
+                    if temp == k:
+                        temp, count = 0, count + 1
+                else:
+                    temp = 0
+            return count >= m
+
+        low, high = min(bloomDay), max(bloomDay)
+        ans = -1
+        while low <= high:
+            mid = (low + high) // 2
+            print(low, mid, high)
+            if isValid(mid):
+                ans = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+        return ans
+    
+    
+bloomDay = [1,10,3,10,2]
+m = 3
+k = 1
+
+obj = Solution()
+
+print(obj.minDays(bloomDay, m, k))    
