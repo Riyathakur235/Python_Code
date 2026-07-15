f6eedcf8907dba574fc79b12ee90bb6a540b293a
Riyathakur235 <riyathakur8052@gmail.com>
2026-07-15 21:54:38 +0530

    int

--- Loops.py
+++ Loops.py
@@ -54,7 +54,7 @@
 #         print("*",end="")
 #    print("")    
 
-reverse of table
-n=int(input("Enter the number: "))
-for i in range(1,11):
-    print(f"{n} x {11-i} ={n*(11-i)}")
+# reverse of table
+# n=int(input("Enter the number: "))
+# for i in range(1,11):
+#     print(f"{n} x {11-i} ={n*(11-i)}")
