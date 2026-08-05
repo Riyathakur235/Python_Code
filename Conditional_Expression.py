986c866fecf00e72681bd6f5eb5ff7bfd4c7d79e
Riyathakur235 <riyathakur8052@gmail.com>
2026-08-05 19:02:42 +0530

    class

--- Conditional_Expression.py
+++ Conditional_Expression.py
@@ -2,6 +2,7 @@
 # marks2 =int(input("Enter marks2: "))
 # marks3 =int(input("Enter marks3: "))
 
+
 # # check for total percentage
 # total_percentage = (100*(marks1+ marks2+ marks3))/300
 # if(total_percentage>=40 and marks1>=33 and marks2>=33,marks3>=33):
--- Genrate_table.py
+++ Genrate_table.py
@@ -48,8 +48,7 @@
 #   if("python" in line):
 #      print(f"Yes,python is present. line no.{lineno}") 
 #      break
-#   lineno+=1  
-  
+#   lineno+=1    
 # else:
 #     print("No,python is not present") 
 
