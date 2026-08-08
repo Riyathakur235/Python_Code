4aad063f93d6a29f40821ed7d980bc5275f7a10a
Riyathakur235 <riyathakur8052@gmail.com>
2026-08-08 22:00:38 +0530

    star

--- space.py
+++ space.py
@@ -1,9 +1,19 @@
-n=5
-for i in range(n):
-    for j in range(2*n-1):
-        if(i==0 or i==n-1 or j==0 or j==2*n-2 or
-            i+j==n-1 or i-j==n-1 + n):
-            print("*", end=" ")
-        else:
-            print(" ", end=" ")    
-    print()        
+# n=5
+# for i in range(n):
+#     for j in range(2*n-1):
+#         if(i==0 or i==n-1 or j==0 or j==2*n-2 or
+#             i+j==n-1 or i-j==n-1 + n):
+#             print("*", end=" ")
+#         else:
+#             print(" ", end=" ")    
+#     print()    
+    
+
+# star shape pattern
+n = 5
+
+for i in range(1, n + 1):
+    print(" " * (n - i) + "* " * i)
+
+for i in range(n - 1, 0, -1):
+    print(" " * (n - i) + "* " * i)
