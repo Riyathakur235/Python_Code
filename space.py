c70b794c753721eaff5e22dc4eb814890eea49ce
Riyathakur235 <riyathakur8052@gmail.com>
2026-08-11 20:44:56 +0530

    num

--- space.py
+++ space.py
@@ -10,8 +10,16 @@
     
 
 # star shape pattern
-n = 5
-for i in range(1, n + 1):
-    print(" " * (n - i) + "* " * i)
-for i in range(n - 1, 0, -1):
-    print(" " * (n - i) + "* " * i)
+# n = 5
+# for i in range(1, n + 1):
+#     print(" " * (n - i) + "* " * i)
+# for i in range(n - 1, 0, -1):
+#     print(" " * (n - i) + "* " * i)
+
+
+nums = [1,2,3,4,5]
+for num in nums:
+    if num == 3:
+        break
+    else:
+        print("Loop completed")
