10b08ce1d500ce5bfbfac5fd96b26ecb0a55935a
Riyathakur235 <riyathakur8052@gmail.com>
2026-09-14 22:04:37 +0530

    manage

--- Small_Project/Management.py
+++ Small_Project/Management.py
@@ -9,4 +9,29 @@ def add_student():
         "name" : name,
         "age" : age
     })
-    print("Student ")
+    print("Student added successfully!")
+    
+def view_students():
+    if len(students) ==0:
+        print("No student found.")
+    else:
+        print("\nStudents List:")
+        for Student in students:
+            print("Name:" , Student["name"])
+            print("Age:" , Student["age"])
+
+while True:
+    print("\n1. Add Student")
+    print("2. View Students")
+    print("3. Exit")
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        add_student()
+    elif choice == "2":
+        view_students()
+    elif choice == "3":
+        print("Exiting...")
+        break
+    else:
+        print("Invalid choice. Please try again.")
