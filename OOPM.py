2d5d2004d064f806f4ba971726e94565a64ed70d
Riyathakur235 <riyathakur8052@gmail.com>
2026-09-22 21:19:50 +0530

    oop

--- OOPM.py
+++ OOPM.py
@@ -93,18 +93,18 @@
 # a.hello()
 
 
-from random import randint, random
-class Train:
-    def __init__(self,trainNo):
-        self.trainNo =trainNo
-    def book(self,fro,to):
-        print(f"Ticket is booked in train no: {self.trainNo} from{fro} to{to}")
-    def getStatus(self):
-        print(f"Train no: {self.trainNo} is running on time")
-    def getFare(self,fro,to):
-        print(f"Ticket is fare in train no: {self.trainNo} from{fro} to {to} is {randint(189,555)}")
+# from random import randint, random
+# class Train:
+#     def __init__(self,trainNo):
+#         self.trainNo =trainNo
+#     def book(self,fro,to):
+#         print(f"Ticket is booked in train no: {self.trainNo} from{fro} to{to}")
+#     def getStatus(self):
+#         print(f"Train no: {self.trainNo} is running on time")
+#     def getFare(self,fro,to):
+#         print(f"Ticket is fare in train no: {self.trainNo} from{fro} to {to} is {randint(189,555)}")
 
-t=Train(12398)
-t.book("Rampur","Delhi")
-t.getStatus()
-t.getFare("Rampur","Delhi")        
+# t=Train(12398)
+# t.book("Rampur","Delhi")
+# t.getStatus()
+# t.getFare("Rampur","Delhi")        
